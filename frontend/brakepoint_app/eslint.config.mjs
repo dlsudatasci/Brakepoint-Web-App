6480896f9f30848e0afe39396c542eb4daebb0d2
BautistaRalph <bautista.ralphgabriel@gmail.com>
2026-05-08 10:44:25 +0800

    fix: automated testing errors

--- frontend/brakepoint_app/eslint.config.mjs
+++ frontend/brakepoint_app/eslint.config.mjs
@@ -5,14 +5,23 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unescaped-entities": "warn",
+      "react/display-name": "warn",
+      "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "import/no-anonymous-default-export": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
